dcb17bbd9d06174628a3f9f982a0e62e282f25cc
Imanol Eizaguirre <imanol@imanol.cc>
2011-06-12 19:38:49 +0200

    PEP8 changes

--- lastfm-advanced-charts/core/charts.py
+++ lastfm-advanced-charts/core/charts.py
@@ -1,23 +1,44 @@
 # encoding: utf-8
 from time import gmtime
-from lib.GChartWrapper import Sparkline, Line
+from GChartWrapper import Sparkline
+
 
 def axis_dates(dates):
+    """
+    Generate the data for the axis
+    """
     rs = []
     for elem in dates:
         rs.append(gmtime(float(elem[1])))
     return rs
 
-def regla3(dato,maximo):
-    return 100*dato / maximo
 
-def getCharts(d1,legend,color,width,height,line_g,bg,bgcolor,grid_strong,dates,maxvalue):
-    
+def regla3(dato, maximo):
+    """
+    Generates proportional data
+    """
+    return (100 * dato) / maximo
+
+
+def getCharts(d1,
+             dates,
+             maxvalue,
+            color = u'0077CC',
+             width = 1000,
+             height = 300,
+             line_g = 2,
+             bg = u'yes',
+             bgcolor = u'E6F2FA',
+             grid_strong = 1):
+    """
+    Generates the graph url
+    """
+
     values = d1.values()
-    
+
     l1 = []
     calendar = {}
-    
+
     #Dates
     calendar[1] = 'Jan'
     calendar[2] = 'Feb'
@@ -42,46 +63,55 @@ def getCharts(d1,legend,color,width,height,line_g,bg,bgcolor,grid_strong,dates,m
     calendar[2010] = '10'
     calendar[2011] = '11'
     calendar[2012] = '12'
-    
-    
-    if (maxvalue != ''):
+    calendar[2013] = '13'
+    calendar[2014] = '14'
+    calendar[2015] = '15'
+    calendar[2016] = '16'
+    calendar[2017] = '17'
+    calendar[2018] = '18'
+
+    if maxvalue != '':
         topL = int(maxvalue)
     else:
         topL = int(max(values))
-    
-    if (topL==0):
+
+    if topL == 0:
         topL = 1
-    
+
     for elem in values:
-        l1.append(regla3(elem,topL))
-    
+        l1.append(regla3(elem, topL))
+
     inicio1 = dates[0][0]
-    med1 = dates[int(round(len(dates)/2,0))][0]
-    fin1 = dates[len(dates)-1][1]
-    
-    #xlabel = axis_dates(dates)
-    
+    med1 = dates[int(round(len(dates) / 2, 0))][0]
+    fin1 = dates[len(dates) - 1][1]
+
     inicio = gmtime(float(inicio1))
     med = gmtime(float(med1))
     fin = gmtime(float(fin1))
-    
-    p1 =str(calendar[int(inicio[1])])+' '+str(calendar[int(inicio[0])])
-    p2 =str(calendar[int(med[1])])+' '+str(calendar[int(med[0])])
-    p3 =str(calendar[int(fin[1])])+' '+str(calendar[int(fin[0])])
-    
-    
-    L = Sparkline(l1,encoding='text')
-    #L = Line(l1,encoding='text')
-    L.grid(2,10,1,2)
+
+    p1 = '%s %s' % (calendar[int(inicio[1])], calendar[int(inicio[0])])
+    p2 = '%s %s' % (calendar[int(med[1])], calendar[int(med[0])])
+    p3 = '%s %s' % (calendar[int(fin[1])], calendar[int(fin[0])])
+
+    L = Sparkline(l1, encoding=u'text')
+    L.grid(2, 10, 1, 2)
     L.color(color)
-    L.size(width,height)
-    if (bg=='yes'):
-        L.marker('B', bgcolor,0,0,0)
-    L.line(line_g,grid_strong,0)
+    L.size(width, height)
+
+    if bg == u'yes':
+        L.marker('B', bgcolor, 0, 0, 0)
+
+    L.line(line_g, grid_strong, 0)
     L.axes.type('xy')
-    #L.legend('legend')
-    L.axes.label(1, '0',int(round(topL/8,0)),int(round(topL/4,0)),int(round(topL/2.5,0)),int(round(topL/2.0)),int(round(topL/1.6,0)),int(round(topL/1.33,0)),int(round(topL/1.14,0)),topL) 
-    L.axes.label(0,p1,p2,p3)
-    #L.axes.label(0,*xlabel)
+    L.axes.label(1, '0',
+                        int(round(topL / 8, 0)),
+                        int(round(topL / 4, 0)),
+                        int(round(topL / 2.5, 0)),
+                        int(round(topL / 2.0)),
+                        int(round(topL / 1.6, 0)),
+                        int(round(topL / 1.33, 0)),
+                        int(round(topL / 1.14, 0)), topL)
+
+    L.axes.label(0, p1, p2, p3)
 
     return L
--- lastfm-advanced-charts/core/lastfm-advanced-charts.py
+++ lastfm-advanced-charts/core/lastfm-advanced-charts.py
@@ -1,12 +1,11 @@
 # encoding: utf-8
 
-#from urllib import urlopen
-import logging
 from urllib import urlopen
 from lib.BeautifulSoup import BeautifulSoup
 
 LASTFM_API_KEY = 'your_api_key'
 LASTFM_SECRET = 'your_api_secret'
+
 
 def prepare(dates, limite):
     """
@@ -16,16 +15,17 @@ def prepare(dates, limite):
     a1 = longitud % limite
     a2 = longitud / limite
     total = a2
-    if (a1 !=0):
-        a3 = longitud - a2*limite
-        total+=1
+    if a1 != 0:
+        #a3 = longitud - a2 * limite
+        total += 1
     return total
+
 
 def getDates(user):
     """
     Obtains given user's weekly charts data
     """
-    url_dates = u'http://ws.audioscrobbler.com/2.0/?method=user.getweeklychartlist&user='+user+'&api_key='+settings.LASTFM_API_KEY
+    url_dates = u'http://ws.audioscrobbler.com/2.0/?method=user.getweeklychartlist&user=%s&api_key=%s' % (user, LASTFM_API_KEY)
     f = urlopen(url_dates)
     soup = BeautifulSoup(f)
     f.close()
@@ -33,21 +33,23 @@ def getDates(user):
     dates = []
 
     for c in chart:
-        dates.append([c['from'],c['to']])
+        dates.append([c['from'], c['to']])
 
     return dates
 
-def getDates_weeks(user,weeks=52):
+
+def getDates_weeks(user, weeks=52):
     dates = getDates(user)
     longitud = len(dates)
-    if (longitud<=weeks):
+    if longitud <= weeks:
         inicio = 0
     else:
-        inicio = longitud-weeks
+        inicio = longitud - weeks
 
     return dates[inicio:longitud]
 
-def getArtistsGraph(artist,user,dates):
+
+def getArtistsGraph(artist, user, dates):
     """
     Obtains scrobblings for the given band
     """
@@ -55,32 +57,34 @@ def getArtistsGraph(artist,user,dates):
 
     #g1 = artist.decode('utf-8').lower()
     g1 = artist.lower()
-    a1 = {0:0}
+    a1 = {0: 0}
     i = 0
 
     for elem in dates:
-        i = i+1
-        a1[i] = a1[i-1]
+        i += 1
+        a1[i] = a1[i - 1]
 
         nfrom = elem[0]
         nto = elem[1]
-        url_chart = u'http://ws.audioscrobbler.com/2.0/?method=user.getweeklyartistchart&user='+user+'&from='+nfrom+'&to='+nto+'&api_key='+settings.LASTFM_API_KEY
+        url_chart = u'http://ws.audioscrobbler.com/2.0/?method=user.getweeklyartistchart&user=%s&from=%s&to=%s&api_key=%s' % (user. nfrom, nto, LASTFM_API_KEY)
         f = urlopen(url_chart)
         soup = BeautifulSoup(f.read())
         f.close()
-        aux = soup.findAll({'name':True})
-        aux2 = soup.findAll({'playcount':True})
+        aux = soup.findAll({'name': True})
+        aux2 = soup.findAll({'playcount': True})
 
         for elem in range(len(aux)):
             if (aux[elem].renderContents().decode('utf-8').lower() == g1):
-                a1[i] = int(a1[i-1]) + int(aux2[elem].renderContents().decode('utf-8'))
+                a1[i] = int(a1[i - 1]) + int(aux2[elem].renderContents().decode('utf-8'))
                 break
     return a1
 
-def getImg(artist,how_many):
-    #Fallan los artistas con espacios
 
-    url = 'http://ws.audioscrobbler.com/2.0/?method=artist.getimages&artist='+artist+'&limit='+str(how_many)+'&order=popularity&api_key='+settings.LASTFM_API_KEY
+def getImg(artist, how_many):
+    """
+    Adds an image of the artist to the result set
+    """
+    url = 'http://ws.audioscrobbler.com/2.0/?method=artist.getimages&artist=%s&limit=%s&order=popularity&api_key=%s' % (artist, how_many, LASTFM_API_KEY)
     img = []
     aux = []
 
@@ -91,49 +95,10 @@ def getImg(artist,how_many):
 
         aux = soup.findAll('size')
     except Exception, e:
-        print 'getImg Exception: '+str(e)
+        print 'getImg Exception: %s' % e
 
     for c in aux:
         if (c['name'] == u'largesquare'):
             img.append(c.renderContents())
 
     return img
-
-def getArtistsGraph2(g1,user,a1,dates,indice):
-
-    soup = None
-    a1[0] = 0
-    m = indice
-    i = 0
-
-    for elem in dates:
-        i = i+1
-        m = m+1
-
-        nfrom = elem[0]
-        nto = elem[1]
-        url_chart = u'http://ws.audioscrobbler.com/2.0/?method=user.getweeklyartistchart&user='+user+'&from='+nfrom+'&to='+nto+'&api_key='+settings.LASTFM_API_KEY
-
-        try:
-            f = urlopen(url_chart)
-            soup = BeautifulSoup(f.read())
-            f.close()
-        except Exception, e:
-            print 'getArtistGraph Exception 1: '+str(e)
-
-        aux = soup.findAll({'name':True})
-        aux2 = soup.findAll({'playcount':True})
-
-        try:
-            for elem in range(len(aux)):
-                if (aux[elem].renderContents().decode('utf-8') == g1.decode('utf-8')):
-                    a1[m] = int(a1[m-1]) + int(aux2[elem].renderContents().decode('utf-8'))
-                try:
-                    a1[m]
-                except:
-                    a1[m] = a1[m-1]
-
-        except Exception, e:
-            logging.error(u'getArtistGraph Exception 2: %s' % str(e))
-    return a1
-
